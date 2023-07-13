ade2d7f1cf44e49aa4dcef414bcfda5990a2d6a3
jungseo <jungwook22@gmail.com>
2023-07-13 14:02:33 +0900

    간단한 주석 설명 추가

--- src/test/java/com/example/jwttokenpractice/auth/JwtTokenizerTest.java
+++ src/test/java/com/example/jwttokenpractice/auth/JwtTokenizerTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.springframework.expression.ExpressionException;
-
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
@@ -20,7 +18,7 @@ public class JwtTokenizerTest {
     private String secretKey;
     private String base64EncodedSecretKey;
 
-//    (1)
+//    (1) 테스트 케이스에서 사용할 Secret Key를 Base64 형식으로 인코딩
     @BeforeAll
     public void init() {
         jwtTokenizer = new JwtTokenizer();
@@ -29,7 +27,7 @@ public class JwtTokenizerTest {
         base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(secretKey);
     }
 
-//    (2)
+//    (2) Plain Text인 Secret Key가 Base64 형식으로 인코딩이 정상적으로 수행이 되는지 테스트
     @Test
     public void encodeBase64SecretKeyTest() {
         System.out.println(base64EncodedSecretKey);
@@ -37,7 +35,7 @@ public class JwtTokenizerTest {
         assertThat(secretKey).isEqualTo(new String(Decoders.BASE64.decode(base64EncodedSecretKey)));
     }
 
-//    (3)
+//    (3) JwtTokenizer가 Access Token을 정상적으로 생성하는지 테스트
 @Test
 public void generateAccessTokenTest() {
     String accessToken = getAccessToken(Calendar.MINUTE, 10);
@@ -45,7 +43,7 @@ public void generateAccessTokenTest() {
     assertThat(accessToken).isNotNull();
     }
 
-//    (4)
+//    (4) JwtTokenizer가 Refresh Token을 정상적으로 생성하는지 테스트
     public void generateRefreshTokenTest() {
         String subject = "test refresh token";
         Calendar calendar = Calendar.getInstance();
@@ -59,7 +57,7 @@ public void generateAccessTokenTest() {
         assertThat(refreshToken).isNotNull();
     }
 
-//    (5)
+//    (5) JwtTokenizer의 verifySignature() 메서드가 Signature를 잘 검증하는지 테스트
     @DisplayName("does not throw any Exception when jws verify")
     @Test
     public void verifySignatureTest() {
@@ -67,7 +65,7 @@ public void generateAccessTokenTest() {
         assertDoesNotThrow(() -> jwtTokenizer.verifySignature(accessToken, base64EncodedSecretKey));
     }
 
-//    (6)
+//    (6) JWT 생성 시 지정한 만료일시가 지나면 JWT가 정말 만료되는지 테스트
     @DisplayName("throw ExpiredJwtException when jws verify")
     @Test
     public void verifyExpirationTest() throws InterruptedException{
